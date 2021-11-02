1b09c457cdcdac62b12c5578518a36c8ada1354f
mslmyrtd <mslmyrtd@gmail.com>
2021-11-02 19:09:22 +0300

    new

--- CC-04/index.js
+++ CC-04/index.js
@@ -1,20 +1,18 @@
-const display =document.getElementById("display");
+const display = document.getElementById("display");
 const input = document.getElementById("input");
 const button = document.getElementById("button");
-
-
 button.addEventListener("click", ()=>{
-    var email =input.value;
-    if(!email.includes("@")){
-        display.innerText= `Your ${email} does not include @ `;
-    }
-    var extension = email.split("@")[1].split(".")[1];
-    if(email.includes("@") && extension.length<=3){
-        display.innerText=`${email} is valid`;
-        input.value=""
-    }
-    else
-        return display.innerText=`${email} is invalid`;
     
-   
+    var email = input.value;
+    if(!email.includes("@")){
+        display.innerHTML = `${email} is invalid`;
+    }  
+    var extension = email.split("@")[1].split(".")[1];
+    if(email.includes("@") && extension.length <= 3){
+        display.innerHTML = `${email} is valid`;
+    }
+    else{
+        return  display.innerHTML = `${email} is invalid`;
+    }
+
 })
